083e7c376cc27ad33631850e24ae14ac36725b5b
OniwaPenut <137640432+PenutCreation@users.noreply.github.com>
2025-03-24 17:03:25 +0800

    Delete Series directory

--- Series/SeriesSystem.js
+++
@@ -1,7 +0,0 @@
-$('#sectionChooser').change(function(){
-    var myID = $(this).val();
-    $('.panel').each(function(){
-        myID === $(this).attr('id') ? $(this).show() : $(this).hide();
-    });
-});
-document.getElementById("1").style.display = "block";
